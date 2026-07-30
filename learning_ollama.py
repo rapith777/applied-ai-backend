df1bcf1900987e89c3bdb8b9ed6afc64bc8b151e
RAJESH KUMAR <rajeshpithani777@gmail.com>
2026-07-30 11:28:07 +0200

    setting up proper functions

--- learning_ollama.py
+++ learning_ollama.py
@@ -1,14 +1,26 @@
 import requests
 
+
 def get_ollama_models():
     url = "http://127.0.0.1:11434/api/tags"
+
     response = requests.get(url, timeout=10)
     response.raise_for_status()
-    data = response.json()
-    return data
+
+    return response.json()
+
+
+def extract_model_names(data):
+    names = []
+
+    for model in data["models"]:
+        names.append(model["name"])
+
+    return names
+
 
 result = get_ollama_models()
+model_names = extract_model_names(result)
 
-for model in result["models"]:
-    print (model["name"])
+print(model_names)
 
